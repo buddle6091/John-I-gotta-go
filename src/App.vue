694a71a7db03802646c0c799428a64bb976a5d1e
buddle6091 <buddle6091@naver.com>
2021-10-06 23:15:59 +0900

    commit test (selectbox)

--- src/App.vue
+++ src/App.vue
@@ -50,11 +50,11 @@
  type='form' text="Return Date" place="Select date"/>
 </div>
 Passengers <Icon icon='person' :style="{width:'0px', height: '0px'}"/>
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-people-fill" viewBox="0 0 16 16">
+<!--   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-people-fill" viewBox="0 0 16 16">
   <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
   <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
   <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
-</svg>
+</svg> -->
  <div class="passengers"> 
   <div class="passengersAdult">
     <div class="detailPass">
@@ -316,9 +316,11 @@ html{
 input[type="radio"]{
   display : none;
 }
+
 .passengers{
   @extend .block;
   width: 100%;
+  max-width: 30rem;
   height: 5rem;
   position: relative;
   display: flex;
@@ -326,9 +328,9 @@ input[type="radio"]{
   justify-content: space-between;
 }
 .detailPass{
- 
   justify-content: center;
-  text-align: right;
+  display: inline;
+  text-align: center;
      h2{
         color: $text-main;
         font: {
