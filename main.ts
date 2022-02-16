66c8f99519104d6af9a97fb89db2592507b1afac
buddle6091 <83018778+buddle6091@users.noreply.github.com>
2022-02-17 00:03:45 +0900

    Delete main.ts

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-//import axios from 'axios'
-import '../css/bootstrap.min.css'
-import store from './store.js'
-
-createApp(App)
-   .use(store)
-   .mount('#app')
